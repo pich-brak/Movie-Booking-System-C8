41778887d703810a43c226b171e62423c1dfc97b
Bopha <khatbopha.095@gmail.com>
2025-06-02 10:49:42 +0700

    correct code in class Person

--- src/Person.ts
+++ src/Person.ts
@@ -2,13 +2,11 @@ export abstract class Person {
     private id: number;
     private name: string;
     private email: string;
-    private password: string;
 
-    constructor(id: number, name: string, email: string, password: string) {
+    constructor(id: number, name: string, email: string) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.password = password;
     }
 
      register(): void {
