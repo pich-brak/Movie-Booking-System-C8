d94de13fd525f6013d4a619bccdc71b25b261fef
Bopha <khatbopha.095@gmail.com>
2025-06-02 19:41:32 +0700

    create class Staff and complete the code

--- src/Staff.ts
+++ src/Staff.ts
@@ -1,7 +1,18 @@
 import { Person } from "./Person";
-
-exprot class Staff extends Person {
+export class Staff extends Person {
     constructor(id: number, name: string, email: string) {
         super(id, name, email);
     }
+
+     scanTicket(ticket: Ticket): boolean {
+    if (ticket.valid && !ticket.used) {
+      ticket.used = true;
+      console.log(`Ticket #${ticket.id} scanned successfully by ${this.name}.`);
+      return true;
+    } else {
+      console.log(`Ticket #${ticket.id} is invalid or already used.`);
+      return false;
+    }
+  }
+
 }
